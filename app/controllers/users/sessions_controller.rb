85f9bde31f99a2c49ebdde28565b97a561d78c2c
vep002 <vep002@gmail.com>
2024-06-17 13:43:34 -0500

    write action for sign up, login, logout

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,27 +2,35 @@
 
 class Users::SessionsController < Devise::SessionsController
   respond_to :json
-  # before_action :configure_sign_in_params, only: [:create]
 
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  private
 
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def respond_with(current_user, _opts = {})
+    render json: {
+      status: {
+        code: 200, message: 'Logged in successfully.',
+        data: {user: UserSerializer.new(current_user).serializable_hash[:data][:attributes]}
+      }
+    }, status: :ok
+  end
 
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+  def respond_to_on_destroy
+    if request_headers['Authorization'].present?
+      jwt_payload = JWT.decode(request.headers['Authorization'].split(' ').last, Rails.application.credentials.devise_jwt_secret_key!).first
+      current_user = User.find(jwt_payload['sub'])
+    end
 
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+    if current_user
+      render json: {
+        status: 200,
+        message: 'Logged out successfully.'
+      }, status: :ok
+    else 
+      render json: {
+        status: 401,
+        message: 'Could not find an active user session.'
+      }, status: :unauthorized
+    end
+  end
+ 
 end
